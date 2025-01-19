3028043cf089794cef12dbf3d371aa6c3ec9af37
Sathish Kumar <158498273+sathixh@users.noreply.github.com>
2025-01-19 19:59:11 +0530

    Add files via upload

---
+++ src/components/Nav.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='h-24 bg-white flex justify-center'>
+        <div className='flex m-8 mr-12'>
+            <div className='text-green-700 mr-4'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10"><path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" /></svg>
+            </div>
+            <div className=''>
+                <p className='font-bold'>FREE DELIVERY</p>
+                <p className="text-gray-600 text-sm">For all order over</p>
+            </div>
+        </div>
+        <div className='flex m-8 mr-12'>
+            <div className='text-green-700 mr-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>
+            </div>
+            <div className=''>
+                <p className='font-bold'>90 DAYS RETURN</p>
+                <p className="text-gray-600 text-sm">If goods have problems</p>
+            </div>
+        </div>
+        <div className='flex m-8 mr-12'>
+            <div className='text-green-700 mr-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" /></svg>
+            </div>
+            <div className=''>
+                <p className='font-bold'>SECURE PAYMENT</p>
+                <p className="text-gray-600 text-sm">100% secure payment</p>
+            </div>
+        </div>
+        <div className='flex m-8 mr-12'>
+            <div className='text-green-700 mr-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10"><path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" /></svg>
+            </div>
+            <div className=''>
+                <p className='font-bold'>24/7 SUPPORT</p>
+                <p className="text-gray-600 text-sm">Dedicated support</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
